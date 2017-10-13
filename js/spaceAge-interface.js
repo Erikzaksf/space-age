a0c875cc9dfdf32916a7d7ad97b2ba28ea6a5dfc
erikzaksf <erik-zak-sf@gmail.com>
2017-10-13 13:13:56 -0700

    attempt to get npm to build properly

---
+++ js/spaceAge-interface.js
@@ -0,0 +1,13 @@
+
+var YearSeconds = require('./../js/yearsSeconds.js').yearsSecondsModule;
+
+$(document).ready(function() {
+  $("form#regularNumber").submit(function(event) {
+    event.preventDefault();
+    var age = parseInt($("input#number").val());
+    var yearsSeconds = new YearSeconds();
+    var result = yearsSeconds.converter(numberInput);
+    $(".well").show();
+    $(".output").text(result);
+  });
+});
---
+++ js/yearSeconds.js
@@ -0,0 +1,13 @@
+function YearSeconds(age) {}
+
+YearSeconds,prototype.converter = function(num) {
+  let age = "";
+  const secondsInYear = 31556926;
+  let result = result = age * secondsInYear;
+  return result;
+
+
+
+
+}
+exports.yearsSecondsModule = YearSeconds;
