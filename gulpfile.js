3a6b49f08e7118b94bdaa79a7749cdf9605195e7
erikzaksf <erik-zak-sf@gmail.com>
2017-10-23 20:15:35 -0700

    fix incorrect spelling in gulpfile and attempt to properly route module for age in seconds function

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,50 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var babelify = require("babelify");
+var lib = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
+      "main": [
+        "less/bootstrap.less",
+        "dist/css/bootstrap.css",
+        "dist/js/bootstrap.js"
+      ]
+    }
+  }
+});
+var browserSync = require('browser-sync').create();
 
-gulp.task('jsBrowserify', ['concatInterface'], function(){
-  return browserify({ entries: ['./tmp/allConcat.js'] })
+var buildProduction = utilities.env.production;
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
+    .transform(babelify.configure({
+      presets: ["es2015"]
+    }))
     .bundle()
     .pipe(source('app.js'))
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task('minifyScripts', ['jsBrowserify'], function(){
-  return gulp.src('./build/js/app.js')
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+    .pipe(gulp.dest("./build/js"));
 });
 
-gulp.task('clean', function(){
+gulp.task("clean", function(){
   return del(['build', 'tmp']);
 });
 
@@ -31,30 +63,31 @@ gulp.task('jshint', function(){
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('bowerJS', function(){
-  return gulp.src(lib.ext('js').files)
+gulp.task('bowerJS', function () {
+  return gulp.src(lib.ext("js").files)
     .pipe(concat('vendor.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('bowerCSS', function(){
+gulp.task('bowerCSS', function () {
   return gulp.src(lib.ext('css').files)
-  .pipe(concat('vendor.css'))
-  .pipe(gulp.dest('./build/css'));
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('bower', ['bowerJS', 'bowerCSS']);
+gulp.task('bower', ['bowerJS', 'bowerCSS','cssBuild', 'imgBuild']);
 
-gulp.task('serve', function(){
+gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: './',
-      index: 'index.html'
+      baseDir: "./",
+      index: "index.html"
     }
   });
   gulp.watch(['js/*.js'], ['jsBuild']);
   gulp.watch(['bower.json'], ['bowerBuild']);
+
 });
 
 gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
@@ -63,4 +96,16 @@ gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
 
 gulp.task('bowerBuild', ['bower'], function(){
   browserSync.reload();
+});
+
+gulp.task("cssBuild", function() {
+  gulp.src(['css/*.css'])
+  .pipe(concat('app.css'))
+  .pipe(gulp.dest('./build/css'))
+});
+
+gulp.task("imgBuild", function() {
+  gulp.src(['img/*.gif'])
+  .pipe(concat('img.gif'))
+  .pipe(gulp.dest('./build/img'))
 });
--- js/yearSeconds.js
+++ js/yearSeconds.js
@@ -1,13 +1,10 @@
-function YearSeconds(age) {}
-
-YearSeconds,prototype.converter = function(num) {
-  let age = "";
-  const secondsInYear = 31556926;
-  let result = result = age * secondsInYear;
-  return result;
-
-
-
-
-}
-exports.yearsSecondsModule = YearSeconds;
+export class Age {
+  function YearSeconds(age) {
+    YearSeconds.prototype.converter = function(num) {
+      let age = "";
+      const secondsInYear = 31556926;
+      let result = age * secondsInYear;
+    }
+  }return result;
+  // exports.yearsSecondsModule = YearSeconds;
+};
